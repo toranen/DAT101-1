88543f985d8401f5c515ef2b0a237f953a45fd04
atsond12 <atsond12@student.uia.no>
2025-01-07 12:10:08 +0100

    Ny Modul 5

---
+++ module_5/Sonic/sonic.mjs
@@ -0,0 +1,24 @@
+"use strict";
+
+//--------------- Objects and Variables ----------------------------------//
+
+// prettier-ignore
+const SpriteInfoList = {
+  sonic1: { x: 0, y:   0, width: 102, height: 125, count: 5},
+  sonic2: { x: 0, y: 125, width: 102, height: 125, count: 9},
+  sonic3: { x: 0, y: 250, width: 102, height: 125, count: 9},
+  sonic4: { x: 0, y: 375, width: 102, height:  75, count: 7},
+  sonic5: { x: 0, y: 450, width: 102, height: 125, count: 5},
+  sonic6: { x: 0, y: 575, width: 102, height: 125, count: 3},
+  sonic7: { x: 0, y: 700, width: 102, height: 125, count: 2},
+  sonic8: { x: 0, y: 825, width: 102, height: 125, count: 2},
+  sonic9: { x: 0, y: 950, width: 102, height: 125, count: 4}
+}
+
+//--------------- Functions ----------------------------------------------//
+
+//--------------- Event Handlers -----------------------------------------//
+
+//--------------- Main Code ----------------------------------------------//
+
+
