7bb8efc456b5a72fda7da59e2f6f5f27761d07ab
atsond12 <atsond12@student.uia.no>
2025-01-07 14:38:24 +0100

    Oppdatering Workshop 5

---
+++ common/libs/libSprite.mjs
@@ -0,0 +1,48 @@
+"use strict";
+
+/**
+ * @library libSprite
+ * @description A library for classes that manage sprite animations.
+ */
+
+class TSpriteCanvas {
+  #cvs;
+  #ctx;
+  #img;
+
+  constructor(aCanvas) {
+    this.#cvs = aCanvas;
+    this.#ctx = aCanvas.getContext("2d");
+    this.#img = new Image();
+  }
+
+  loadSpriteSheet(aFileName, aLoadedFinal) {
+    this.#img.onload = aLoadedFinal;
+    this.#img.src = aFileName;
+  }
+
+  drawSprite(aSpriteInfo, aDx = 0, aDy = 0, aIndex = 0) {
+    let index = aIndex;
+    const sx = aSpriteInfo.x + (index * aSpriteInfo.width);
+    const sy = aSpriteInfo.y;
+    const sw = aSpriteInfo.width;
+    const sh = aSpriteInfo.height;
+    const dx = aDx;
+    const dy = aDy;
+    const dw = sw;
+    const dh = sh;
+    this.#ctx.drawImage(this.#img, sx, sy, sw, sh, dx, dy, dw, dh);
+  }
+}
+
+export default {
+  /**
+   * @class TSpriteCanvas
+   * @description A class that manage sprite canvas for lading sprite sheets.
+   * @param {HTMLCanvasElement} aCanvas - The canvas element to use.
+   * @function loadSpriteSheet - Loads a sprite sheet image.
+   * @param {string} aFileName - The file name of the sprite sheet image.
+   * @param {function} aLoadedFinal - A callback function to call when the image is done loading.
+   */
+  TSpriteCanvas: TSpriteCanvas,
+};
--- module_5/Sonic/sonic.mjs
+++ module_5/Sonic/sonic.mjs
@@ -1,10 +1,10 @@
 "use strict";
-
+import libSprite from "../../common/libs/libSprite.mjs";
 //--------------- Objects and Variables ----------------------------------//
 
 // prettier-ignore
 const SpriteInfoList = {
-  sonic1: { x: 0, y:   0, width: 102, height: 125, count: 5},
+  sonic1: { x: 0, y:   0, width: 102, height: 125, count: 6},
   sonic2: { x: 0, y: 125, width: 102, height: 125, count: 9},
   sonic3: { x: 0, y: 250, width: 102, height: 125, count: 9},
   sonic4: { x: 0, y: 375, width: 102, height:  75, count: 7},
@@ -15,10 +15,16 @@ const SpriteInfoList = {
   sonic9: { x: 0, y: 950, width: 102, height: 125, count: 4}
 }
 
+const cvs = document.getElementById("cvs");
+const spriteCanvas = new libSprite.TSpriteCanvas(cvs);
+spriteCanvas.loadSpriteSheet("./Media/sonic_sprite_sheet.png", onLoaded);
 //--------------- Functions ----------------------------------------------//
 
 //--------------- Event Handlers -----------------------------------------//
-
+function onLoaded(){
+  console.log("Sprite sheet loaded.");
+  spriteCanvas.drawSprite(SpriteInfoList.sonic1);
+}
 //--------------- Main Code ----------------------------------------------//
 
 
