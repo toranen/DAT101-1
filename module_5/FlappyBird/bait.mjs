b99b730f04c43faefb3c421240697207147ccdcf
atsond12 <atsond12@student.uia.no>
2025-01-21 13:16:14 +0100

    Bait implementasjon

---
+++ module_5/FlappyBird/bait.mjs
@@ -0,0 +1,8 @@
+"use strict";
+import libSprite from "../../common/libs/libSprite.mjs";
+
+export class TBait extends libSprite.TSprite{
+  constructor(aSpriteCanvas, aSpriteInfo, aPosition){
+    super(aSpriteCanvas, aSpriteInfo, aPosition);
+  }
+}
